e4ef15a3055cb03e609d928fbd4b855bce87f9f0
Charithz <charith.ddm@gmail.com>
2025-10-07 00:15:51 +0530

    employee added

--- src/main/java/com/cdz/repository/UserRepository.java
+++ src/main/java/com/cdz/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.cdz.repository;
 
+import com.cdz.model.Branch;
+import com.cdz.model.Store;
 import com.cdz.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
     User findByEmail(String username);
+
+    List<User> findByStore(Store store);
+
+    List<User> findByBranchId(Long branchId);
 
 
 }
